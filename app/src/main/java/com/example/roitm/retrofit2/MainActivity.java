a064411219a3dc378e420c47e22bba42a6b5467c
Ilya Roitman <roitman81@gmail.com>
2018-11-13 18:51:45 +0200

    my first commit

--- app/src/main/java/com/example/roitm/retrofit2/MainActivity.java
+++ app/src/main/java/com/example/roitm/retrofit2/MainActivity.java
@@ -36,7 +36,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity
 {
     EditText name;
-    EditText update_name_category;
+    EditText update_name_category, update_id_category;
     EditText idCategory;
      CategoryAdapter adapter;
      RecyclerView recyclerView;
@@ -73,6 +73,8 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onResponse(Call<ArrayList<Category>> call, Response<ArrayList<Category>> response) {
                 generateCategoryList(response.body());
+
+
             }
 
             @Override
@@ -83,17 +85,20 @@ public class MainActivity extends AppCompatActivity
 
         });
 
-
-
-
-
     }
 
     /** Method to generate List of notice using RecyclerView with custom adapter*/
     private void generateCategoryList(ArrayList<Category> categoryArrayList) {
 
+        Toast.makeText(this, categoryArrayList.get(1).getName(),Toast.LENGTH_LONG).show();
+
+
         recyclerView = findViewById(R.id.recycler_view_notice_list);
+
         adapter = new CategoryAdapter(categoryArrayList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
 
 
         add=findViewById(R.id.buttonNew);
@@ -102,16 +107,15 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v) {
                 name_Category = name.getText().toString();
-                //  id_Category=idCategory.getText().toString();
+                  id_Category=idCategory.getText().toString();
                 Category category = new Category();
-                // category.setId((id_Category));
+                 category.setId((id_Category));
                 category.setName(name_Category);
-
                 list.add(category);
                 adapter.notifyDataSetChanged();
                 Toast.makeText(MainActivity.this,"User Add Success...",Toast.LENGTH_SHORT).show();
-                name.setText("");
-                idCategory.setText("");
+                name.setText(" ");
+                idCategory.setText(" ");
 
             }
         });
@@ -129,31 +133,24 @@ public class MainActivity extends AppCompatActivity
                 dialog.show();
             }
         });
-
-
-
-
-
-
 }
-
-
-
-
-
-
     private void InitUpdateDialog(final int position, View view) {
 
         update_name_category = view.findViewById(R.id.update_name_category);
+        update_id_category=view.findViewById(R.id.update_id_category);
         btnUpdate = view.findViewById(R.id.btn_update_user);
         btnCancel = view.findViewById(R.id.btn_update_cancel);
         update_name_category.setText(name_Category);
+        update_id_category.setText(id_Category);
+
         btnUpdate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 name_Category = update_name_category.getText().toString();
+                id_Category=update_id_category.getText().toString();
                 Category category = new Category();
                 category.setName(name_Category);
+                category.setId(id_Category);
                 adapter.UpdateData(position, category);
                 Toast.makeText(MainActivity.this, "User Updated..", Toast.LENGTH_SHORT).show();
             }
@@ -165,7 +162,53 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-    }}
+    }
+    public void sendPost(String id, String name) {
+        Category.savePost(id, name, 1).enqueue(new Callback<Category>() {
+            @Override
+            public void onResponse(List<Category> call, Response<Category> response) {
+
+                if(response.isSuccessful()) {
+                    showResponse(response.body().toString());
+                    Log.i(TAG, "post submitted to API." + response.body().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Category> call, Throwable t) {
+                Log.e(TAG, "Unable to submit post to API.");
+            }
+        });
+    }
+
+    public void showResponse(String response) {
+        if(update_name_category.getVisibility() == View.GONE) {
+            update_name_category.setVisibility(View.VISIBLE);
+        }
+        update_name_category.setText(response);
+    }
+    public void sendPost(String title, String body) {
+
+        // RxJava
+        Category.savePost(title, body, 1).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Category>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Category category) {
+                        showResponse(category.toString());
+                    }
+                });
+    }
+}
 
 
 
--- app/src/main/java/com/example/roitm/retrofit2/my_interface/GetCategoryDataService.java
+++ app/src/main/java/com/example/roitm/retrofit2/my_interface/GetCategoryDataService.java
@@ -9,10 +9,14 @@ import com.example.roitm.retrofit2.model.Category;
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 
-public interface GetCategoryDataService
-{
-    @GET("/MyPortfolio2/rest/category")
+public interface GetCategoryDataService {
+    @GET("MyPortfolio2/rest/category")
     Call<ArrayList<Category>> getCategoryData();
+
+    @PUT("MyPortfolio2/rest/category")
+    Call<Category> UpdateData(@Body Category category);
 }
